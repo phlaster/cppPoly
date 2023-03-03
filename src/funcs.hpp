dfd7365da286c6888697827cd175d216b159dabf
Alex <curjucklloyd@gmail.com>
2023-03-03 21:43:50 +0300

    Initial commit:     + main.cpp     + funcs.cpp with SICP-style iterative sqrt function     + funcs.hpp

---
+++ src/funcs.hpp
@@ -0,0 +1,6 @@
+#ifndef FUNCS_HPP
+#define FUNCS_HPP
+
+double sqrt(double x, double eps=1e-9, double init_guess=1.0);
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "funcs.hpp"
+using namespace std;
+
+int main(){
+	double n = 0.0;
+	cout << "Ented number to take sqrt" << endl;
+	cin >> n;
+	cout << sqrt(n) << endl;
+	return 0;
+}
