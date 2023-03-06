ae30f8ebb2dbf8f82af2ba32bf48becffa32bafc
Alex <curjucklloyd@gmail.com>
2023-03-06 03:58:51 +0300

    refactored entry point

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
+#include <vector>
 #include <iostream>
-#include "sqrt.hpp"
+#include "headers/solve.hpp"
+#include "headers/print.hpp"
 using namespace std;
 
+
 int main(){
-	double n = 0.0;
-	cout << "Ented number to take sqrt" << endl;
-	cin >> n;
-	cout << sqrt(n) << endl;
+	double a = 0;
+	double b = 0;
+	double c = 0;
+    input(&a,&b,&c);
+    print(solve(a,b,c));
 	return 0;
 }
