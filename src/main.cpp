29238abf2c71b407c7739d93727483573595cb6a
Alex <curjucklloyd@gmail.com>
2023-03-06 04:24:28 +0300

    remove redundant includes

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,12 @@
-#include <vector>
-#include <iostream>
 #include "headers/solve.hpp"
 #include "headers/print.hpp"
-using namespace std;
 
 
 int main(){
-	double a = 0;
-	double b = 0;
-	double c = 0;
+    double a = 0;
+    double b = 0;
+    double c = 0;
     input(&a,&b,&c);
     print(solve(a,b,c));
-	return 0;
+    return 0;
 }
