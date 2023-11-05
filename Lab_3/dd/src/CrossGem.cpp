ebb71af50a32f54f8fa2ecd013f28b68478fddf4
Alex <curjucklloyd@gmail.com>
2023-11-06 01:07:38 +0300

    rewrote cross activation

--- Lab_3/dd/src/CrossGem.cpp
+++ Lab_3/dd/src/CrossGem.cpp
@@ -11,9 +11,9 @@ CrossGem::CrossGem(float x, float y, SpriteEnum sprite, SpriteEnum bonus)
     this->m_cross.setTexture(&Gem::SPRITE);
     this->m_cross.setTextureRect(
         sf::IntRect(
-            static_cast<int>(Gem::ONE_SPRITE_WIDTH * static_cast<int>(bonus)),
-            static_cast<int>(Gem::ONE_SPRITE_WIDTH),
-            static_cast<int>(Gem::ONE_SPRITE_WIDTH), static_cast<int>(Gem::ONE_SPRITE_WIDTH)
+            int(Gem::ONE_SPRITE_WIDTH * int(bonus)),
+            int(Gem::ONE_SPRITE_WIDTH),
+            int(Gem::ONE_SPRITE_WIDTH), int(Gem::ONE_SPRITE_WIDTH)
         )
     );
 
@@ -30,24 +30,27 @@ void CrossGem::draw(sf::RenderWindow* window) const {
     window->draw(this->m_cross);
 }
 
+
 int CrossGem::activate(GemTable& gems, size_t i, size_t j, size_t width, size_t height) {
-    int xi = static_cast<int>(i) + ((rand() % 2) ? -1 : 1);
-    int xj = static_cast<int>(j) + ((rand() % 2) ? -1 : 1);
-    int xi1 = static_cast<int>(i) + ((rand() % 2) ? -1 : 1);
-    int xj1 = static_cast<int>(j) + ((rand() % 2) ? -1 : 1);
+    int dx[8] = { -1, 1, -1, 1, -2, 2, -2, 2};
+    int dy[8] = { -1, -1, 1, 1, -2, 2,  2, -2};
 
-    while (xi == xi1) xi1 = static_cast<int>(i) + ((rand() % 2) ? -1 : 1);
-    while(xj == xj1) xj1 = static_cast<int>(j) + ((rand() % 2) ? -1 : 1);
+    for (int k = 0; k < 8; k++) {
+        int xi = i + dx[k];
+        int xj = j + dy[k];
 
-    if (xi >= 0 && xj >= 0 && xi < height && xj < width && gems.at(xi).at(xj)->getSprite() != SpriteEnum::SpriteEmpty)
-        gems.at(xi).at(xj)->setSprite(m_bonus);
-
-    if (xi1 >= 0 && xj1 >= 0 && xi1 < height && xj1 < width && gems.at(xi1).at(xj1)->getSprite() != SpriteEnum::SpriteEmpty)
-        gems.at(xi1).at(xj1)->setSprite(m_bonus);
-
+        if (
+            xi >= 0 &&
+            xi < height &&
+            xj >= 0 &&
+            xj < width &&
+            gems.at(xi).at(xj)->getSprite() != SpriteEnum::SpriteEmpty) {
+            gems.at(xi).at(xj)->setSprite(m_bonus);
+        }
+    }
     sf::Vector2f pos = this->getPosition();
     delete gems.at(i).at(j);
     gems.at(i).at(j) = new Gem(pos.x, pos.y, SpriteEnum::SpriteEmpty);
 
-    return static_cast<int>(ScoreEnum::RewardX);
+    return int(ScoreEnum::RewardX);
 }
