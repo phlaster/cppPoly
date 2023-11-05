f82e399157e9effd8a93262122d218cc19bb788e
Alex <curjucklloyd@gmail.com>
2023-11-06 01:07:55 +0300

    now expand on hover

--- Lab_3/dd/src/Gem.cpp
+++ Lab_3/dd/src/Gem.cpp
@@ -13,8 +13,8 @@ Gem::Gem(float x, float y, SpriteEnum sprite) : m_sprite(sprite) {
     this->m_gem.setTexture(&Gem::SPRITE);
     this->m_gem.setTextureRect(
         sf::IntRect(
-            static_cast<int>(Gem::ONE_SPRITE_WIDTH * static_cast<int>(sprite)), 0,
-            static_cast<int>(Gem::ONE_SPRITE_WIDTH), static_cast<int>(Gem::ONE_SPRITE_WIDTH)
+            int(Gem::ONE_SPRITE_WIDTH * int(sprite)), 0,
+            int(Gem::ONE_SPRITE_WIDTH), int(Gem::ONE_SPRITE_WIDTH)
         )
     );
 
@@ -25,8 +25,8 @@ void Gem::setSprite(SpriteEnum sprite) {
     this->m_sprite = sprite;
     this->m_gem.setTextureRect(
         sf::IntRect(
-            static_cast<int>(Gem::ONE_SPRITE_WIDTH * static_cast<int>(sprite)), 0,
-            static_cast<int>(Gem::ONE_SPRITE_WIDTH), static_cast<int>(Gem::ONE_SPRITE_WIDTH)
+            int(Gem::ONE_SPRITE_WIDTH * int(sprite)), 0,
+            int(Gem::ONE_SPRITE_WIDTH), int(Gem::ONE_SPRITE_WIDTH)
         )
     );
 }
@@ -38,9 +38,9 @@ int Gem::activate(GemTable& gems, size_t i, size_t j, size_t width, size_t heigh
 
 void Gem::hover(bool isHover) {
     if (isHover)
-        this->m_gem.setScale(0.9f, 0.9f);
+        this->m_gem.setScale(1.2, 1.2);
     else
-        this->m_gem.setScale(1.f, 1.f);
+        this->m_gem.setScale(1.0, 1.0);
 }
 
 sf::Vector2f Gem::getPosition() const {
